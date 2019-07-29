0f3f69dea7b28045bb368aa114d7c1eb39893879
rdavid1099 <rdavid1099@gmail.com>
2019-07-29 11:26:27 -0700

    Solve exercise

--- rotational-cipher/lib/rotational_cipher.ex
+++ rotational-cipher/lib/rotational_cipher.ex
@@ -7,6 +7,31 @@ defmodule RotationalCipher do
   "Nggnpx ng qnja"
   """
   @spec rotate(text :: String.t(), shift :: integer) :: String.t()
+  def rotate(text, 0), do: text
+
   def rotate(text, shift) do
+    String.to_charlist(text)
+      |> shift_charlist(shift)
   end
+
+  defp shift_charlist(chars, shift) do
+    Enum.map(chars, &convert_char(&1, shift))
+      |> to_string
+  end
+
+  defp convert_char(char, shift) when char >= ?a and char <= ?z do
+    case rem((char + shift), ?z + 1) do
+      remainder when remainder < ?a -> remainder + ?a
+      remainder -> remainder
+    end
+  end
+
+  defp convert_char(char, shift) when char >= ?A and char <= ?Z do
+    case rem((char + shift), ?Z + 1) do
+      remainder when remainder < ?A -> remainder + ?A
+      remainder -> remainder
+    end
+  end
+
+  defp convert_char(char, _), do: char
 end
