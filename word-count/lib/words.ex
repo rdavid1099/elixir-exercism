180f69d588e69128240356e60a49bc7ecb54fbab
rdavid1099 <rdavid1099@gmail.com>
2019-07-29 14:24:35 -0700

    Complete word count exercise

--- word-count/lib/words.ex
+++ word-count/lib/words.ex
@@ -6,5 +6,23 @@ defmodule Words do
   """
   @spec count(String.t()) :: map
   def count(sentence) do
+    String.split(sentence, ~r/[\s_]/)
+      |> Enum.reduce(%{}, &collect_word_count(&1, &2))
+  end
+
+  defp collect_word_count(word, result) do
+    sanitize(word)
+      |> update_word_count(result)
+  end
+
+  defp sanitize(word) do
+    Regex.replace(~r/[[:punct:]]+$/u, word, "")
+      |> String.downcase
+  end
+
+  defp update_word_count("", result), do: result
+
+  defp update_word_count(word, result) do
+    Map.update(result, sanitize(word), 1, &(&1 + 1))
   end
 end
