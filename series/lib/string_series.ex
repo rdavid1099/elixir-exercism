5e1b6a8638b0c51c6f388f411a8e2d9bcd21de7f
rdavid1099 <rdavid1099@gmail.com>
2019-07-29 17:36:04 -0700

    Complete series exercise

--- series/lib/string_series.ex
+++ series/lib/string_series.ex
@@ -5,6 +5,20 @@ defmodule StringSeries do
   return an empty list.
   """
   @spec slices(s :: String.t(), size :: integer) :: list(String.t())
-  def slices(_s, _size) do
+  def slices(_, size) when size < 1, do: []
+
+  def slices(s, size) do
+    if size <= String.length(s) do
+      sanitized_size = size - 1
+      iterate_n_times(String.length(s) - sanitized_size, s, size)
+    else
+      []
+    end
+  end
+
+  defp iterate_n_times(0, _, _, result), do: Enum.reverse(result)
+
+  defp iterate_n_times(n, <<_>> <> tail = str, size, result \\ []) do
+    iterate_n_times(n - 1, tail, size, [String.slice(str, 0..size - 1) | result])
   end
 end
